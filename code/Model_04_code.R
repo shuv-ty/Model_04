9fabd13079b6a7a7b687bc68f70062b6f41d5a60
shuv-ty <st9523@gmail.com>
2025-06-24 00:24:05 +0530

    Final code push 2

--- code/Model_04_code.R
+++ code/Model_04_code.R
@@ -1,3 +1,7 @@
+library(readxl)
+credit_card <- read_excel("data/credit_card.xls")
+View(credit_card)
+
 #Import all important library
 
 library(tidyverse)
@@ -8,24 +12,22 @@ View(credit_card)
 dim(credit_card)
 str(credit_card)
 
-#change the target variablbe into factor variable
+#change the target variable into factor variable
 
 credit_card$default <- as.factor(credit_card$default)
 
 #Run GLM for default variable using all independent variable
 
-model_check <- glm(default ~ . , data = credit_card, family = binomial)
-summary(model_check)
+model_check_01 <- glm(default ~ . , data = credit_card, family = binomial)
+summary(model_check_01)
 
 #drop those variable which are insignificant
 
 credit_card <- credit_card %>% 
-  select(-PAY_3, -PAY_6, -PAY_5, -PAY_6,
+  select(-ID, -SEX, -EDUCATION, -AGE, -MARRIAGE, 
+         -PAY_3, -PAY_6, -PAY_5, -PAY_6,
          -BILL_AMT1, -BILL_AMT2, -BILL_AMT3, -BILL_AMT4, -BILL_AMT5, BILL_AMT6,
          -PAY_AMT3, -PAY_AMT4, -PAY_AMT5, -PAY_AMT6)
-
-credit_card <- credit_card %>% 
-  select(-SEX, -EDUCATION, -MARRIAGE)
 
 view(credit_card)
 
@@ -42,26 +44,34 @@ split <- sample.split(credit_card$default, SplitRatio = 0.8)
 train <- subset(credit_card, split == TRUE)
 test <- subset(credit_card, split == FALSE)
 
-#Now train the GLM model in train dataset
-
-
+#Now train the GLM model in train data set
 
 model <- glm(default ~. , data = train, family = binomial)
 summary(model)
 
-plot(model)
+#Now Predict "default" based on model trained
 
 prob <- predict(model, newdata = test, type = "response")
 
-pred_class <- ifelse(prob > 0.65, 1, 0)
+#set threshold value 
+pred_class <- ifelse(prob > 0.6, 1, 0)
 
 actual <- test$default
 predicted <- pred_class
 
 table(actual , predicted)
 
+#check all the important evaluated data
+
 library(caret)
 confusionMatrix(table, positive = "1")
+
+##########################
+#accuracy is     [81%]       which is good, that can be improve after
+#including other variables 
+
+
+#plotting matrix
 
 barplot(table,
         beside = TRUE,
@@ -70,6 +80,18 @@ barplot(table,
         xlab = "Predicted",
         ylab = "Count",
         main = "Actual vs Predicted (Classification)")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
